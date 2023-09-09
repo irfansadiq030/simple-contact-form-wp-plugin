65e8afb9d89a5d62086ff919fa38b65e3995b145
irfansadiq <32975448+irfansadiq030@users.noreply.github.com>
2023-09-09 15:04:03 +0500

    custom post type created
    - Created and Registered Custom post type with name "Contact Form"

--- simple-contact-form.php
+++ simple-contact-form.php
@@ -24,10 +24,43 @@ class Simple_Contact_Form
     // Constructor Method
     public function __construct()
     {
-        add_action('init', array($this, 'custom_post'));
+        add_action('init', array($this, 'create_custom_post_type'));
     }
 
-    public function custom_post()
+    public function create_custom_post_type()
     {
+        $labels = array(
+            'name'                  => 'Contact Form',
+            'singular_name'         => 'Contact Form Entry',
+            'menu_name'             => 'Contact Form', 
+            'name_admin_bar'        => 'Contact Form', 
+            'add_new'               => 'Add New Contact Entry',
+            'add_new_item'          => 'Add New Contact Entry',
+            'new_item'              => 'New Contact Entry',
+            'edit_item'             => 'Edit Contact Entry',
+            'view_item'             => 'View Contact Entry',
+            'all_items'             => 'All Contact Entries',
+            'search_items'          => 'Search Contact Entries',
+            'not_found'             => 'No entry found.', 
+            'not_found_in_trash'    => 'No entries found in Trash.',
+            'items_list'            => 'Contact Entries list',
+        );
+
+        $args = array(
+            'labels'              => $labels,
+            'public'              => true,
+            'publicly_queryable'  => false,
+            'exclude_from_search' => true,
+            'capability_type'     => 'post',
+            'capability'          => 'manage_options',
+            'menu_position'       => null,
+            'menu_icon'       => 'dashicons-media-text',
+            'supports'            => array('title'),
+        );
+
+        // Registering Post Type
+        register_post_type( 'simple_contact_form', $args );
     }
 }
+
+new Simple_Contact_Form();
